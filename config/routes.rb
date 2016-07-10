b5bf665d0a425251938d98e60863f8e3f1684cb8
Katya Dreyer-Oren <kdreyeroren@gmail.com>
2016-07-10 14:20:25 -0400

    protecting my cancan sitch before maybe destroying everything

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :products
+  resources :products # delete this so not everyone can add products
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'products#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
