2264698248dda5d0c0268040858bc99b16a49224
Katya Dreyer-Oren <kdreyeroren@gmail.com>
2016-07-12 18:29:12 -0400

    added photo uploader

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,10 @@ class Product < ActiveRecord::Base
 
   validates_presence_of :price
   validates_presence_of :name
-  belongs_to :admin_user
 
-#  mount_uploader :photo, PhotoUploader
+
+  mount_uploader :photo, ProductphotoUploader
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711161824) do
+ActiveRecord::Schema.define(version: 20160712214725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160711161824) do
     t.decimal  "price",      precision: 20, scale: 4
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "photo"
   end
 
   create_table "users", force: :cascade do |t|
