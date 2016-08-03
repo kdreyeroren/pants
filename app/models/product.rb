943a6bd55cb691619d1a4976f5a93de8c106f62d
Katya Dreyer-Oren <kdreyeroren@gmail.com>
2016-08-03 11:27:33 -0400

    working on stripe still

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
   validates_presence_of :price
   validates_presence_of :name
 
+  has_many :users, through: :orders
+  has_many :orders
 
   mount_uploader :photo, ProductphotoUploader
 
